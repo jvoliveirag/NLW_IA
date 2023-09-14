0fea4352ee31cf10638acd5e4683589d0e72cbc0
jvoliveirag <jv.oliveira@gmail.com>
2023-09-14 11:02:05 -0300

    Class 02 [Part 3]: Completion route and CORS.

--- nlw_ai_api/src/server.ts
+++ nlw_ai_api/src/server.ts
@@ -1,13 +1,19 @@
+import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify()
 
+app.register(fastifyCors, {
+  origin: "*", //em prod colocar exatamente o endereço do front
+})
 app.register(getAllPromptsRoute) //todos os modulos (getAllPromptsRoute, nesse caso) usando o register devem ser asincronos
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
   port:3333,
