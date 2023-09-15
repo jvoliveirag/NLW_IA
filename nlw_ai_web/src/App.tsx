d5a167e83ba9a6a1d28241f0043179d64ac5ddfd
jvoliveirag <jv.oliveira@gmail.com>
2023-09-15 14:36:50 -0300

    Class 03 [Part 2]: Finishing the Frontend.

--- nlw_ai_web/src/App.tsx
+++ nlw_ai_web/src/App.tsx
@@ -1,5 +1,9 @@
+import { useCompletion } from "ai/react";
 import { Github, Wand2 } from "lucide-react";
+import { useState } from "react";
+import { PromptSelect } from "./components/prompt-select";
 import { Button } from "./components/ui/button";
+import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Separator } from "./components/ui/separator";
 import { Slider } from "./components/ui/slider";
@@ -7,6 +11,21 @@ import { Textarea } from "./components/ui/textarea";
 import { VideoInputForm } from "./components/video-input-form";
 
 export function App() {
+
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+  
+  const { input, setInput, handleInputChange, handleSubmit, completion, isLoading } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-type': 'application/json',
+    }
+  })
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -37,11 +56,14 @@ export function App() {
             <Textarea 
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea 
               className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..." 
               readOnly
+              value={completion}
             />
           </div>
 
@@ -51,27 +73,19 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator/>
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
 
             <div className="space-y-2">
-              <label htmlFor="">Prompt</label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt"/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <Label>Prompt</Label>
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className="space-y-2">
-              <label htmlFor="">Modelo</label>
+              <Label>Modelo</Label>
               <Select disabled defaultValue="gpt3.5">
                 <SelectTrigger>
                   <SelectValue/>
@@ -88,12 +102,14 @@ export function App() {
             <Separator/>
 
             <div className="space-y-4">
-              <label htmlFor="">Temperatura</label>
+              <Label>Temperatura: <span className="border border-slate-800 rounded-sm px-1 text-base">{temperature}</span></Label>
               <Slider
                 className="cursor-pointer"
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo (porém com possíveis erros).
@@ -102,7 +118,7 @@ export function App() {
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2"/>
             </Button>
