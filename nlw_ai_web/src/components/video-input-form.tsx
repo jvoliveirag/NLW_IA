471dcaefcbd8d4c98d879261a5de3e8ca9f13f74
jvoliveirag <jv.oliveira@gmail.com>
2023-09-15 14:34:56 -0300

    Class 03 [Part 1]: Creating components for prompt and video select.

--- nlw_ai_web/src/components/video-input-form.tsx
+++ nlw_ai_web/src/components/video-input-form.tsx
@@ -1,3 +1,4 @@
+import { api } from "@/lib/axios";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 import { FileVideo, Upload } from "lucide-react";
@@ -7,9 +8,24 @@ import { Label } from "./ui/label";
 import { Separator } from "./ui/separator";
 import { Textarea } from "./ui/textarea";
 
-export function VideoInputForm() {
+type Status = 'waiting' | 'uploading' | 'converting' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!',
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
 
   const [videoFile, setVideoFile] = useState <File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
+
   const promptInputRef = useRef <HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -74,10 +90,28 @@ export function VideoInputForm() {
       return
     }
     
+    setStatus('converting')
     //converter o video em audio (por conta do tamanho de arquivo suportado e valocidade)
     const audioFile = await convertVideoToAudio(videoFile)
+    //console.log(audioFile, prompt)
 
-    console.log(audioFile, prompt)
+    const data = new FormData()
+
+    data.append('file', audioFile)
+
+    setStatus('uploading')
+    const response = await api.post('/videos', data)
+
+    const videoId = response.data.video.id
+
+    setStatus('generating')
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt,
+    })
+
+    setStatus('success')
+
+    props.onVideoUploaded(videoId)
   }
 
   const previewURL = useMemo(() => { // previewURL so vai ser recarregada SOMENTE se o videoFile mudar
@@ -112,15 +146,26 @@ export function VideoInputForm() {
         <Label htmlFor="transcription-prompt">Prompt de transcrição</Label>
         <Textarea
           ref={promptInputRef}
+          disabled={status !== 'waiting'}
           id="transcription-prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
         />
       </div>
       
-      <Button type="submit" className="w-full">
-        Carregar vídeo
-        <Upload className="w-4 h-4 ml-2"/>
+      <Button 
+        data-success={status == 'success'}
+        disabled={status !== 'waiting'} 
+        type="submit" 
+        className="w-full data-[success=true]:bg-emerald-400"
+      >
+        { status == 'waiting' ? (
+          <>
+            Carregar vídeo
+            <Upload className="w-4 h-4 ml-2"/>
+          </>
+          ) : statusMessages[status]
+        }
       </Button>
     </form>
   )
